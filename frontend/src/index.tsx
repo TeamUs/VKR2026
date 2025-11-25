af785c5848800aca000e8f9a12e67adbeca5fc4a
TeamUs <temapavsok@gmail.com>
2025-11-26 00:07:41 +0300

    Latest design updates

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { StyleSheetManager } from 'styled-components';
 import App from './App';
 import './fonts.css';
 
@@ -7,8 +8,22 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+// Create a stylesheet element for styled-components
+let styleSheet: HTMLStyleElement | null = null;
+if (typeof document !== 'undefined') {
+  styleSheet = document.createElement('style');
+  styleSheet.setAttribute('data-styled', 'active');
+  document.head.appendChild(styleSheet);
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    {styleSheet ? (
+      <StyleSheetManager sheet={styleSheet.sheet as any}>
+        <App />
+      </StyleSheetManager>
+    ) : (
+      <App />
+    )}
   </React.StrictMode>
 );
